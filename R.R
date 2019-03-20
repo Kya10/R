a65d0e24932f1d732ef403bedb0b9114a1058fbc
Kya10 <48753048+Kya10@users.noreply.github.com>
2019-03-20 13:31:41 +0300

    Add files via upload

---
+++ R.R
@@ -0,0 +1,48 @@
+## Introduction to R
+## Livingston Kyalo
+## 20th March 2019
+## introduction to simple arthimetics
+
+# Addition ----
+a<-100+50
+a
+b<-123+567
+b
+c<-129756+54972639
+c
+# subtraction ----
+d<-5789-2536
+d
+e<-100500-100700
+e
+## division ----
+f<-100/10
+f
+g<-13879/35647
+g
+h<-7901/1994
+h 
+## mutiplication ----
+i<-6594*25
+i
+j<-21042017*2.5
+j
+##power----
+k<- 2536^3
+k
+l<-567^9
+l
+m<- 25^25
+m
+n<-100^11
+n
+o<- 1e+22
+o
+2+2
+120*120
+
+23896+658-98474
+
+## Random operations ----
+45 * (9/5)
+129-29*20/4
